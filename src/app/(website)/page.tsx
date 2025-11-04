2be71303e74effc12646502939f4e67df6054352
Md.Habibulla <mdhabibulla146425@gmail.com>
2025-11-05 03:48:32 +0600

    feat: new footer add

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -3,6 +3,7 @@ import MyAchievement from "@/components/home/MyAchievement";
 import OurClientSay from "@/components/home/OurClientSay";
 import WhyChooseUs from "@/components/home/WhyChooseUs";
 import HomeServices from "@/components/services/HomeSerivces";
+import Footer from "@/components/shared/Footer";
 import NavBar from "@/components/shared/NavBar";
 import Image from "next/image";
 
@@ -11,10 +12,10 @@ export default function Home() {
     <div className="">
       <MyAchievement />
       <HomeAbout />
-      
       <HomeServices />
       <WhyChooseUs />
       <OurClientSay />
+      <Footer />
     </div>
   );
 }
