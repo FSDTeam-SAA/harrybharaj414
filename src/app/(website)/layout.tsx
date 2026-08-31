a214ee479500f3a187d2eebb02536cb6704bbb14
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2026-08-31 16:56:21 +0600

    feat: implement website layout with SEO metadata, Google Tag integration, and global providers

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "../globals.css";
 import TanStackProvider from "@/provider/tanstackProvider";
 import { Toaster } from "sonner";
@@ -31,6 +32,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18408427055"
+          strategy="afterInteractive"
+        />
+        <Script id="google-tag-aw-18408427055" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-18408427055');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
